38c728e4d7c5315739d490fc39437aee94c86068
bengold1999 <bengold1999@gmail.com>
2024-02-28 17:37:06 +0200

    Add imgs to dom

--- js/galleryController.js
+++ js/galleryController.js
@@ -1,3 +1,13 @@
 'use strict'
 
+renderGallery()
 
+function renderGallery(){
+    var gallery = document.querySelector('.gallery')
+    var strHtml=
+     `
+    <img src="img/2.jpg" alt="no-photo">
+    <img src="img/3.jpg" alt="no-photo">
+    `
+    gallery.innerHTML=strHtml
+}
